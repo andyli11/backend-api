53236b7194f51c10cf8512508b3bd3da29ef56d6
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2021-01-16 01:35:42 -0500

    Add firebase

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+const {
+  db
+} = require('../utils/firebase');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  let promise = new Promise((resolve, reject) => {
+    db.collection("test").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+      });
+    });
+    resolve();
+  });
+
+  await promise.then(() => {
+    res.json({
+      response: true
+    });
+  });
+
 });
 
 module.exports = router;
